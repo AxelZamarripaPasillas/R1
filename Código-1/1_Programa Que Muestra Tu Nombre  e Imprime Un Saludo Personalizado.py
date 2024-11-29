b70a1dc5c9c002b4ad9f27fe59c92e669987652c
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 16:39:20 -0600

    Update and rename 1_Programa Que Muestra Tu Nombre .py to 1_Programa Que Muestra Tu Nombre  e Imprime Un Saludo Personalizado.py

---
+++ Código-1/1_Programa Que Muestra Tu Nombre  e Imprime Un Saludo Personalizado.py
@@ -0,0 +1,6 @@
+# PROGRAMA QUE MUESTRA TU NOMBRE E IMPRIME UN SALUDO PERSONALIZADO
+# Fecha:20241014.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+nombre = input ("nombre:  ")
+print ("Hola", nombre)
