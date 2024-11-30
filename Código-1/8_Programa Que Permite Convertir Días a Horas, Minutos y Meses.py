e6b12dfa67738b2d3b1c57d3f94fb2bf05cbbc1c
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 19:38:34 -0600

    Update 8_Programa Que Permite Convertir Días a Horas, Minutos y Meses.py

--- Código-1/8_Programa Que Permite Convertir Días a Horas, Minutos y Meses.py
+++ Código-1/8_Programa Que Permite Convertir Días a Horas, Minutos y Meses.py
@@ -1,1 +1,13 @@
+# PROGRAMA QUE PERMTE CONVERTIR DÍAS A HORAS, MINUTOS Y MESES
+# Fecha:20241015.
+# Elaborado Por: Axel Zamarripa Pasillas.
 
+dias = int(input("Solicita El Numero De Días: "))
+horas = int ((dias*24))
+minutos = int ((horas*60))
+meses = float ((dias/30))
+
+print ("El Numero De Días Es: " + str(dias))
+print ("El Numero De Horas Es: " + str(horas))
+print ("El Numero De Minutos Es: " + str( minutos ))
+print ("El Numero De Meses Es: " + str(meses))
