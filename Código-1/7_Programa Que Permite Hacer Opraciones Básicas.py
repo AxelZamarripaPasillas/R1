4da2f2a1972a0738633605f50135c6bfc8e3e8cd
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 19:37:55 -0600

    Update 7_Programa Que Permite Hacer Opraciones Básicas.py

--- Código-1/7_Programa Que Permite Hacer Opraciones Básicas.py
+++ Código-1/7_Programa Que Permite Hacer Opraciones Básicas.py
@@ -1,1 +1,15 @@
+# PROGRAMA QUE PERMTE HACER OPERACIONES BASICAS (+, -, * y /)
+# Fecha:20241015.
+# Elaborado Por: Axel Zamarripa Pasillas.
+num1 = int(input("Ingresa El Primer Numero:"))
+num2 = int(input("Ingresa El Segundo Numero:"))
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2 
+
+print ("El valor De La Suma Es:"+ str(suma))
+print ("El valor De La Resta Es:"+ str(resta))
+print ("El valor De La Multiplicación Es:"+ str(multiplicacion))
+print ("El valor De La División Es:"+ str(division))
 
