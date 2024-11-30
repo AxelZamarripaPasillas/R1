c5687ea2e814de9c7f5b9c913a93d3d89593cb62
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 19:41:06 -0600

    Update 11_Programa Para Probrar Las Compuertas Lógica: And, Or y Not.py

--- Código-1/11_Programa Para Probrar Las Compuertas Lógica: And, Or y Not.py
+++ Código-1/11_Programa Para Probrar Las Compuertas Lógica: And, Or y Not.py
@@ -1,1 +1,20 @@
+# Programa Para Probar Las Compuertas Lógicas AND, OR y NOT
+# Fecha:20241018.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+print ("Compuerta OR")
+print (False or False) #False
+print (False or True) #True
+print (True or False) #True
+print (True or True) #True
+
+print ("\n Compuerta AND")
+print (False and False) #False
+print (False and True) #False
+print (True and False) #False
+print (True and True) #True
+
+print ("\n Compuerta NOT")
+print (not(False)) #True
+print (not(True)) #False
 
