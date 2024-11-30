6d40cc132ccdcbcb740bf58bc0035c707b8f89a1
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 19:40:30 -0600

    Update 10_Programa Que Muestra La Lógica Booleana.py

--- Código-1/10_Programa Que Muestra La Lógica Booleana.py
+++ Código-1/10_Programa Que Muestra La Lógica Booleana.py
@@ -1,1 +1,29 @@
+# PROGRAMA QUE MUESTRA LA LÓGICA BOOLEANA (==, !=, >, <, <=, >=)
+# Fecha:20241018
+# Elaborado Por: Axel Zamarripa Pasillas.
 
+print ("----Operador IGUAL (==)")
+
+print (5==5) #True
+print (10==7) #False
+print (5.0==5) #True
+
+print ("\n----Operador DIFERENTE O NO ES IGUAL (!=)")
+print (5 != 5) #False
+print (10 != 7)  #True
+print (5.0 != 5) #False
+
+print ("\n----Operador MAYOR QUE (>)")
+print (10 > 5) #True
+print (5 > 10) #False
+
+print ("\n----Operador MENOR QUE (<)")
+print (5 < 10) #True
+print (10 < 5) #False
+
+print ("\n----Operador MENOR O IGUAL QUE (<=)")
+print (5 <= 5) #True
+print (10 <= 7) #False
+print (3 <= 5) #True
+
+print ("\u263A"*10)
