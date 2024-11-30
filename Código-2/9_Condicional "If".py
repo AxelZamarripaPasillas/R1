cc958b2b41230ccc3346c09cbc1de225d9e3c561
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 21:05:16 -0600

    Create 9_Condicional "If".py

---
+++ Código-2/9_Condicional "If".py
@@ -0,0 +1,12 @@
+# CONDICIONAL IF.
+# Fecha:20241025.
+# Elaborado Por: Axel Zamarripa Pasillas.
+cantidad = int (input("¿Cuántos Artículos Compró?"))
+
+if cantidad >3:
+    total= cantidad * 30
+else:
+    total = cantidad * 45
+
+print ("El Total a Pagar Es $" + str(total))
+print("Saludo...")
