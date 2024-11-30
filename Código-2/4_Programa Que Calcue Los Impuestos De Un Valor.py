ef787d510165be51a39d1326ac5ff30996cd7050
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 20:49:31 -0600

    Update 4_Programa Que Calcue Los Impuestos De Un Valor.py

--- Código-2/4_Programa Que Calcue Los Impuestos De Un Valor.py
+++ Código-2/4_Programa Que Calcue Los Impuestos De Un Valor.py
@@ -1,1 +1,12 @@
+# PROGRAMA QUE CALCULA LOS IMPUESTOS DE UN VALOR 
+# Fecha:20241022.
+# Elaborado Por: Axel Zamarripa Pasillas.
 
+#PRIMER PROGRAMA
+valor= (float(input("Ingrese EL Valor: ")))
+valor_con_impuestos = (0.25*valor) 
+print("El Valor De Impuestos Es:  ", valor_con_impuestos)
+print ("¡Gracias Por Usar Nuestro Sistema!")
+
+# SEGUNDO PROGRAMA
+print (f"Los impuestos son: $ {float(input('Ingrese el valor: '))* 0.25:.2f}")
