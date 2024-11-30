8fb0a40e3fcf1d8a9198331a7075b208d72d6816
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 21:02:33 -0600

    Update 6_Subscripting Multiple Characteres (Sustracción).py

--- Código-2/6_Subscripting Multiple Characteres (Sustracción).py
+++ Código-2/6_Subscripting Multiple Characteres (Sustracción).py
@@ -1,1 +1,9 @@
+# SUBSCRIPTING MÚLTIPLE CHARACTERES (SUSTRACCIÓN)
+# Fecha:20241024.
+# Elaborado Por: Axel Zamarripa Pasillas.
 
+word = "hamster"
+print (word[-1])
+print (word[1:-1])
+print (word[-3:])
+print (word[:3])
