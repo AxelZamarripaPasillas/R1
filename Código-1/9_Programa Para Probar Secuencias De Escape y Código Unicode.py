a35e38f477cf7d3688e912e8fc804e3d46cdb743
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 19:39:32 -0600

    Update 9_Programa Para Probar Secuencias De Escape y Código Unicode.py

--- Código-1/9_Programa Para Probar Secuencias De Escape y Código Unicode.py
+++ Código-1/9_Programa Para Probar Secuencias De Escape y Código Unicode.py
@@ -1,1 +1,19 @@
+# PROGRAMA PARA PROBAR SECUENCIAS DE ESCAPE Y UNICODE
+# Fecha:20241016.
+# Elaborado Por: Axel Zamarripa Pasillas.
 
+#Secuencias De Escape
+print ("Hola\tMundo")
+print ("Hola\\Mundo")
+print ("Hola \"Mundo\"")
+print ("Hola\aMundo")
+print ("Hola\bMundo")
+print ("Hola\fMundo")
+print ("Hola\rMundo")
+print ("\Hola\vMundo")
+
+#Emojis De Unicode
+print (" \U0001f600 ")
+print (" \U0001f606 ")
+print (" \U0001f923 ")
+print (" \U0001f496 ")
