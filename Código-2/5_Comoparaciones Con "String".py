273089d2c02f190dad638c9f896659626e53dafe
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 20:53:49 -0600

    Update 5_Comoparaciones Con "String".py

--- Código-2/5_Comoparaciones Con "String".py
+++ Código-2/5_Comoparaciones Con "String".py
@@ -1,1 +1,28 @@
+# COMPARACIONES CON "STRING"
+# Fecha:20241024.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+#Comparación
+print ("perro" == "perro") # True
+print ("perro" != "gato") # True
+print ("Aguascalientes" < "Zacatecas") # False
+print ("Aire" >= "Agua")
+
+# MEMBERSHIP
+# Operador IN
+
+print ("house" in "boathouse") #True
+print ("bien" in "bienvenidos") #True
+print ("y" not in "ejes") #True 
+print ("je" not in "ejes") #False
+
+# SLICING (REBANADOR)
+# Indexing (Indexado)
+mi_nombre = "Choto Chorchis"
+print (mi_nombre [3])
+print (mi_nombre [12])
+
+# Subscripting Múltiple Characteres (Sustracción)
+mi_nombre = "JOSÉ LUIS"
+print (mi_nombre [2:6])
 
