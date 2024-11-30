e98ec0cc971b8913aa231203579ff9d147fcfd8c
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 20:40:58 -0600

    Update and rename 2_Programa Probar Las Compuertas Lógicas AND, OR y NOT.py to 2_Programa Para Probar Las Compuertas Lógicas AND, OR y NOT.py

---
+++ Código-2/2_Programa Para Probar Las Compuertas Lógicas AND, OR y NOT.py
@@ -0,0 +1,17 @@
+# Programa Probar Las Compuertas Lógicas AND, OR y NOT
+# Fecha:20241022.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+# A = False / B= True
+print (False or False) #False
+print (False or True) #True
+print (True or False) #True
+print (True or True) #True
+
+# Ahora Con La Negación (Not)
+# / A / B //not (A or B) /
+# 0 / 0 // 1
+print (not(False or False)) # True
+print (not(False or True)) # False
+print (not(True or False)) # False
+print (not(True or True)) # False
