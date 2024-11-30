1a49ad269535657d9be53bd33f5140714cce1ce4
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 20:41:39 -0600

    Update 1_Compuerta Not.py

--- Código-2/1_Compuerta Not.py
+++ Código-2/1_Compuerta Not.py
@@ -1,1 +1,16 @@
+# COMPUERTA NOT
+# Fecha:20241022.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+# COMPUERTA NOT
+print (not True) # Imprime False
+print (not False) # Imprime True
+
+# NEGAR LA COMPUERTA NOT
+
+print (not(False or False)) # True
+print (not(False or True)) # False
+print (not(True or False)) # False
+print (not(True or True)) # False
+
 
