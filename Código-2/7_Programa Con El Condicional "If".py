ae95853ca79078d11aeb7c01a58b1f5bac60e30b
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 20:48:47 -0600

    Create 7_Programa Con El Condicional "If".py

---
+++ Código-2/7_Programa Con El Condicional "If".py
@@ -0,0 +1,10 @@
+# CONDICIONAL IF
+# Fecha:20241024.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+edad = int (input("INGRESA TU EDAD: "))
+
+if edad >=18:
+    print("Puede Entrar Al Bar")
+if edad <=17:
+    print("No Puede Entrar Al Bar")
