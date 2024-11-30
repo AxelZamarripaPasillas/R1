e05a65e0cc0a968f382b19b752f9adfcac78f8ac
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-29 18:31:07 -0600

    Create 2_Programa Que Muestra El Numero De Días En Una Semana.py

---
+++ Código-1/2_Programa Que Muestra El Numero De Días En Una Semana.py
@@ -0,0 +1,8 @@
+# PROGRAMA QUE MUESTRA EL NUMERO DE DÍAS DE UNA SEMANA
+# Fecha:20241014.
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+print ("¿Cuántos Días Tiene La Semana?")
+
+dias_semana = ("7 días")
+print("El numero de días en una semana son", dias_semana)
