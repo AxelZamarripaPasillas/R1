ea3dc6c6841da4e9adcbc80820f28e53fc6f3848
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-04 20:18:35 -0600

    Update 4_Programa Que Muestra El Nomre De La Figura De Tres Lados.py

--- Código-1/4_Programa Que Muestra El Nomre De La Figura De Tres Lados.py
+++ Código-1/4_Programa Que Muestra El Nomre De La Figura De Tres Lados.py
@@ -2,6 +2,5 @@
 # Fecha:20241014.
 # Elaborado Por: Axel Zamarripa Pasillas.
 
-print ("¿Cuál Es El Nombre De La Figura De Tres Lados?")
-lados = input ()
+lados = input ("¿Cuál Es El Nombre De La Figura De Tres Lados?")
 print ("El Nombre De La Figura De 3 Lados es:", lados )
