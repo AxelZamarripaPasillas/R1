6c013a13747101a625412456c6db5d1d535da78d
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 17:47:42 -0600

    Update 7_Programa Que Permite Calcular El Área y Perimetro De Un CÍrculo.py

--- Código-1/7_Programa Que Permite Calcular El Área y Perimetro De Un CÍrculo.py
+++ Código-1/7_Programa Que Permite Calcular El Área y Perimetro De Un CÍrculo.py
@@ -1,7 +1,3 @@
-# PROGRAMA QUE PERMITE CALCULAR EL PERIMETRO Y EL ÁREA DE UN CÍRCULO
-# Fecha:20241014.
-# Elaborado Por: Axel Zamarripa Pasillas.
-
 print ("Programa Que Permite Calcular El Perimetro y El Área De Un Círculo")
 
 radio = float(input("Ingrese El Radio Del Circulo: "))
