4df539e98f7f73465dc121de3b91167f09695576
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 10:44:41 -0600

    Create 6_Listas.py

---
+++ Código-3/6_Listas.py
@@ -0,0 +1,15 @@
+# Listas
+# Fecha: 20241030
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+# Lista De Enteros
+print ([40, 20, 35])
+
+# Lista De Cadenas De Texto (String) 
+print (["Manzana", "Piña", "Naranja"])
+
+# Lista De Booleanos
+print ([False, True, True])
+
+# Lista Vacía
+print ([])
