f33bf6459a436ad9f9c5c325a11b7d850504344f
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 17:09:01 -0600

    Create 10_List Membership.py

---
+++ Código-3/10_List Membership.py
@@ -0,0 +1,6 @@
+nombres = ["Choto", "Emiliano", "Luis"]
+
+print ("Luis" in nombres) # True
+print ("Emi" in nombres) # False
+print ("Javier" in nombres) # False
+print ("Jose" not in nombres) # True
