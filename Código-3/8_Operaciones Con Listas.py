72faeb10a203ae7ea5f1430098397d842d778fdc
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 11:03:10 -0600

    Update 8_Operaciones Con Listas.py

--- Código-3/8_Operaciones Con Listas.py
+++ Código-3/8_Operaciones Con Listas.py
@@ -1,1 +1,14 @@
+print ("-----INDEXING")
 
+numero = [10, 20, 30]
+print (numero[2])
+print (numero[0])
+print (numero[1])
+print (numero[-1])
+
+print ("\n-----SUBSCRIPTING")
+
+nombre = ["Chorchis", "Choto", "Emiliano", "Pepe"]
+print (nombre [1:])
+print (nombre [1:5])
+print (nombre [-2:])
