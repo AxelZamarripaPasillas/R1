f0ee685f3392e6e5e612c64472b2753cf3b596b8
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 17:47:07 -0600

    Update 5_Programa Que Permite Saber a Cuántos  Metros Equivalen 250 cm.py

--- Código-1/5_Programa Que Permite Saber a Cuántos  Metros Equivalen 250 cm.py
+++ Código-1/5_Programa Que Permite Saber a Cuántos  Metros Equivalen 250 cm.py
@@ -1,7 +1,3 @@
-# PROGRAMA QUE PERMITE SABER A CUÁNTOS METROS EQUIVALEN 250 cm
-# Fecha:20241014.
-# Elaborado Por: Axel Zamarripa Pasillas.
-
 print ("¿A Cuántos Metros Equivalen 250 cm?")
 
 distancia = ("2.5 m")
