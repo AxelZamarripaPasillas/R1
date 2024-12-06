249a52428df64e62b7a654cda58cefeb105c3f10
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-05 21:08:30 -0600

    Update and rename 1_Mascotas,py to 1_Mascota.py

---
+++ Código-3/1_Mascota.py
@@ -0,0 +1,15 @@
+# Mascotas
+# FECHA: 20241205
+# ELABORADO POR: Axel Zamarripa Pasillas.
+
+mascota = input ("Ingresa El Tipo De Mascota Que Tienes: ")
+if "perro" in mascota:                                              # Pregunta si la palabra "perro" este dentro de mascota
+                                                                    # De la variable "mascota"
+                                                                   # Es un else + if fusionado
+    print ("Es Un Perro")
+elif "gato" in mascota:
+    print ("Tienes  Un Gato ")
+else:
+    print ("Tipo De Mascota Desconocida")
+
+print ("¡Gracias Por Usar Nuestro Programa! ")
