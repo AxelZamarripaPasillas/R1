fdfc97b3d25a81a6e678ea7721eda06fd6c4ad1f
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-05 22:18:16 -0600

    Create 3_Comparadores.py

---
+++ Código-3/3_Comparadores.py
@@ -0,0 +1,15 @@
+# Comparadores
+# Fecha: 20241025
+# Elaborado Por: Axel Zamarripa Pasillas.
+
+n1 = (int(input("Ingresa El Primer Número: ")))
+n2 = (int(input("Ingresa El Segundo Número: ")))
+
+if (n1 > n2):
+    print(str(n1) + " Es Mayor Que " + str(n2))
+elif n1 == n2:
+     print(str(n1) + " Es Igual Que " + str(n2))
+else:
+     print(str(n1) + " Es Menor Que " + str(n2))
+
+print ("¡Gracias Por Usar Nuestro Programa!")
