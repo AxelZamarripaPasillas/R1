97e6c2e449c083c2782193a3dd1ef4b3819288d5
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 17:47:25 -0600

    Update 6_Programa Que Permite Calcular El Área De Un Triángulo.py

--- Código-1/6_Programa Que Permite Calcular El Área De Un Triángulo.py
+++ Código-1/6_Programa Que Permite Calcular El Área De Un Triángulo.py
@@ -1,7 +1,3 @@
-# PROGRAMA QUE PERMITE CALCULAR EL ÁREA DE UN TRIÁNGULO 
-# Fecha:20241014.
-# Elaborado Por: Axel Zamarripa Pasillas.
-
 print ("Solicita La Base y La Altura De Un Triángulo")
 
 base = float(input("Ingrese La Base Del Triángulo: "))
