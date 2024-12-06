b0667a492afab231134c752f739a2a59f3c852a9
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 10:56:03 -0600

    Create 7_Tipos De Datos En Las Listas.py

---
+++ Código-3/7_Tipos De Datos En Las Listas.py
@@ -0,0 +1,14 @@
+enteros = [5,4,8]
+print (enteros)
+print(enteros[0])
+print (type(enteros))
+print (type(enteros[0]))
+
+booleano = [False, True, False]
+print (booleano)
+print(booleano[0])
+print (type(booleano))
+print (type(booleano[0]))
+
+string = ["Juan", "Pedro", "Chuy"]
+print (string [1])
