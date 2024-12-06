3dc18e90b42040b00c3f8838d110f78cfb956d69
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-05 21:47:18 -0600

    Update 2_Identificación De Tipos De Datos str, int y float.py

--- Código-3/2_Identificación De Tipos De Datos str, int y float.py
+++ Código-3/2_Identificación De Tipos De Datos str, int y float.py
@@ -1,1 +1,12 @@
+# Identificación De Tipos De Datos str, int y float.
+# Fecha: 20241023
+# Elaborado Por: Axel Zamarripa Pasillas.
 
+variable = input("Ingresa Tu Edad: ")
+print (type(variable))
+
+variable = int(variable)             # Convierto La Variable a Tipo Entero
+print (type(variable))
+
+variable = float (variable)          # Convierto La Variable a Tipo Flotante
+print (type(variable))
