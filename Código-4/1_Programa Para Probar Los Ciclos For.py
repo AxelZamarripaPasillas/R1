3bf36ead3c8216cc53e5a0561e32d474e0e18fb6
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 18:23:59 -0600

    Create 1_Programa Para Probar Los Ciclos For.py

---
+++ Código-4/1_Programa Para Probar Los Ciclos For.py
@@ -0,0 +1,11 @@
+lista_num = [10, 30, 40, 20, 35, 80]
+print (lista_num [0])
+print (lista_num [1])
+print (lista_num [2])
+print (lista_num [3])
+print (lista_num [4])
+print (lista_num [5])
+
+#Haciendo Lo Mismo Pero Con El Ciclo For
+for i in lista_num:
+    print (i)
