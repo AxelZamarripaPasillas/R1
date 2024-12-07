45e9cbaaf06701bdf44d616fc2b5afedafa23725
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 18:34:52 -0600

    Create 10_Programa para demostrar el funcionamiento de  los métodos upper() y lower().py

---
+++ Código-4/10_Programa para demostrar el funcionamiento de  los métodos upper() y lower().py
@@ -0,0 +1,10 @@
+# Define una cadena de texto
+cadena = "Python es un lenguaje de programación"
+print ("\n", cadena)
+
+cadena_mayusculas = cadena.upper () # Convertimos la cadena a máyuscula
+print ("\n", cadena_mayusculas)
+
+cadena_minusculas = cadena.lower () # Convertimos la cadena a minúsculas
+print ("\n", cadena_minusculas)
+print ("\nFin Del Programa") 
