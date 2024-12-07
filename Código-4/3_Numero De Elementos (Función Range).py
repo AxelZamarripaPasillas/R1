30b718a49b525c5c2594fd9d34f6e9fb1b7b3a50
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 18:27:30 -0600

    Create 3_Numero De Elementos (Función Range).py

---
+++ Código-4/3_Numero De Elementos (Función Range).py
@@ -0,0 +1,19 @@
+print ("Imprime Los Valores Del 0 al 9")
+x = range (10)
+for num in x:
+    print (num)
+
+print ("\nImprime Los Valores Del 5 Al 15")
+x1 = range (5, 16)
+for num in x1:
+    print (num)
+
+print ("\nImprime Los Valores Del 10 Al 20")
+x2 = range (10, 21, 2)
+for num in x2:
+    print (num)
+
+print ("\nImprime Los Valores Del 11 Al 21")
+x3 = range (11, 22, 2)
+for num in x3:
+    print (num)
