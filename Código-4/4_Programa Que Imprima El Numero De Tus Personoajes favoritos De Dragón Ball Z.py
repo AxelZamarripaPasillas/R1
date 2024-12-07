960e6e8afb4ec0091db2d54341b6ce40bb40346e
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-12-06 18:28:17 -0600

    Create 4_Programa Que Imprima El Numero De Tus Personoajes favoritos De Dragón Ball Z.py

---
+++ Código-4/4_Programa Que Imprima El Numero De Tus Personoajes favoritos De Dragón Ball Z.py
@@ -0,0 +1,4 @@
+personajes = ["Goku", "Vegeta", "Trunks", "Piccolo", "Gohan", "Krilin", "Bulma", "Yamcha", "Tenshinhan", "Freezer"]
+
+for i in range(len(personajes)):
+    print(f"Personaje {i+1}: {personajes[i]}")
